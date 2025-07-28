b02252f79d071d4a21eafea4a48c2b424a87b4bb
Kévin Bucher <kevin@MacBookPro-001.lan>
2025-07-28 20:33:27 +0200

    Modif eleventy.js

--- web/.eleventy.js
+++ web/.eleventy.js
@@ -1,10 +1,15 @@
-// web/.eleventy.js
+// Fichier : web/.eleventy.js
+
 module.exports = function(eleventyConfig) {
 
-  // Cette ligne dit à Eleventy de copier le dossier css dans le site final
-  eleventyConfig.addPassthroughCopy("src/assets/css");
+  // Cette ligne copie le dossier des assets (CSS, images...) dans le site final
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
+    // On définit explicitement les formats de templates qu'Eleventy doit reconnaître
+    templateFormats: ["njk", "md", "html"],
+
+    // On définit la structure des dossiers
     dir: {
       input: "src",
       output: "_site"
